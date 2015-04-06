de92d817ce97587f6d27eb3095a2e7ca95561276
Nizobe <nbessi2@uic.edu>
2015-04-06 15:25:04 -0500

    Added Clarifying Comments

--- first_ex.py
+++ first_ex.py
@@ -1,11 +1,13 @@
-from bs4 import BeautifulSoup
-from urllib2 import urlopen
+from bs4 import BeautifulSoup #loading BeautifulSoup class
+from urllib2 import urlopen #loading urlopen function
 
+#useful beccause all links start with this and it saves space
 BASE_URL = "http://www.chicagoreader.com"
 
+#gets the links for the BOC, "best of category"
 def get_categry_links(section_url):
-  html = urlopen(section_url).read()
-  soup = BeautifulSoup(html, "lxml")
-  boccat = soup.find("dl", "boccat")
+  html = urlopen(section_url).read()#open the section_url into html
+  soup = BeautifulSoup(html, "lxml")#instance of BeautifulSoup class. Initialized with html objecct and parsed with lxml
+  boccat = soup.find("dl", "boccat")#in soup, find <dl> element with class "boccat"
   categry_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
   return categry_links
